bad916a98f0d20d395439f52cd3e1077f066ba99
Paul Reiter <reiter.paul@gmail.com>
2021-12-03 12:34:41 +0100

    Add minimal cli interface

--- pagode/main.py
+++ pagode/main.py
@@ -1,2 +1,24 @@
+import argparse
+from pathlib import Path
+
+
+def start_share(directory):
+    print(directory)
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(
+        description="Share a directory for pair programming"
+    )
+    parser.add_argument(
+        "dir", metavar="directory", type=str, help="the directory to share"
+    )
+    args = parser.parse_args()
+    directory = Path(args.dir).resolve()
+    if not directory.is_dir():
+        raise ValueError(f"The given path is not a directory '{directory}'")
+    start_share(directory)
+
+
+if __name__ == "__main__":
+    main()
